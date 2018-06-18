98031cc59b27daf5d868156a455b2d98573f3335
Alex Forbes <alex.forbes@starlingbank.com>
2018-06-18 16:05:55 +0100

    Move EnsureAuth to fileHandler
    Probably needs better name

--- internal/fileHandler.go
+++ internal/fileHandler.go
@@ -8,6 +8,9 @@ import (
 	"log"
 	"path/filepath"
 	"strings"
+	"encoding/json"
+	vaultApi "github.com/hashicorp/vault/api"
+	"reflect"
 )
 
 func ReadFile(path string) (string, error) {
@@ -35,12 +38,13 @@ func walkFile(path string, f os.FileInfo, err error) error {
 	if err != nil {
 		return fmt.Errorf("error reading %s: %s", path, err)
 	}
-	if ! f.IsDir() {
-		dir, file := filepath.Split(path)
-		policyPath := strings.Join(strings.Split(dir, "/")[1:], "/")
-		fmt.Println(file)
-		fmt.Println(policyPath)
+	if f.IsDir() {
+		return nil
 	}
+
+	dir, file := filepath.Split(path)
+	policyPath := strings.Join(strings.Split(dir, "/")[1:], "/")
+	fmt.Printf("path: %s, file: %s\n", policyPath, file)
 
 	return nil
 }
@@ -49,3 +53,89 @@ func PutPoliciesFromDir(path string) error {
 	err := filepath.Walk(path, walkFile)
 	return err
 }
+
+func EnsureAuth(c VaultsmithClient) error {
+	// Ensure that all our auth types are enabled and have the correct configuration
+	authListLive, err := c.ListAuth()
+	if err != nil {
+		return err
+	}
+	log.Printf("live auths: %+v", authListLive)
+
+	// TODO hard-coded hack until we figure out how to structure the configuration
+	s, err := ReadFile("example/sys/auth/approle.json")
+
+	var enableOpts vaultApi.EnableAuthOptions
+	err = json.Unmarshal([]byte(s), &enableOpts)
+	if err != nil {
+		return err
+	}
+
+	// we need to convert to AuthConfigOutput in order to compare with existing config
+	var enableOptsAuthConfigOutput vaultApi.AuthConfigOutput
+	enableOptsAuthConfigOutput, err = ConvertAuthConfigInputToAuthConfigOutput(enableOpts.Config)
+	if err != nil {
+		return err
+	}
+
+	var authListConfigured map[string]*vaultApi.AuthMount
+	authListConfigured = make(map[string]*vaultApi.AuthMount)
+	approle := vaultApi.AuthMount{
+		Type:   "approle",
+		Config: enableOptsAuthConfigOutput,
+	}
+
+	authListConfigured["approle"] = &approle
+
+	// Iterate over the configured auths and ensure they are enabled with the correct config
+	for k, authMount := range authListConfigured {
+		// find in live list
+		// append slash because the config from server includes it
+		path := k + "/"
+		if liveAuth, ok := authListLive[path]; ok {
+			if isConfigApplied(enableOpts.Config, liveAuth.Config) {
+				log.Printf("Configuration for role %s already applied\n", authMount.Type)
+				continue
+			}
+		}
+		log.Printf("Enabling %s\n", authMount.Type)
+		c.EnableAuth(authMount.Type, &enableOpts)
+	}
+
+	for k, authMount := range authListLive {
+		// delete entries not in configured list
+		path := strings.Trim(k, "/")
+		if _, ok := authListConfigured[path]; ok {
+			// present, do nothing
+		} else if authMount.Type == "token" {
+			// cannot be disabled, would give http 400 if attempted
+		} else {
+			log.Printf("Disabling auth type %s\n", authMount.Type)
+			err := c.DisableAuth(authMount.Type)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	return nil
+}
+
+// return true if the localConfig is reflected in remoteConfig, else false
+func isConfigApplied(localConfig vaultApi.AuthConfigInput, remoteConfig vaultApi.AuthConfigOutput) bool {
+	/*
+		AuthConfigInput uses string for int types, so we need to re-cast them in order to do a
+		comparison
+	*/
+
+	converted, err := ConvertAuthConfigInputToAuthConfigOutput(localConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if reflect.DeepEqual(converted, remoteConfig) {
+		return true
+	} else {
+		return false
+	}
+}
+
--- vaultsmith.go
+++ vaultsmith.go
@@ -9,9 +9,6 @@ import (
 	"strings"
 
 	"github.com/starlingbank/vaultsmith/internal"
-	vaultApi "github.com/hashicorp/vault/api"
-	"encoding/json"
-	"reflect"
 )
 
 var flags = flag.NewFlagSet("Vaultsmith", flag.ExitOnError)
@@ -71,91 +68,6 @@ func NewVaultsmithConfig() (*VaultsmithConfig, error) {
 	}, nil
 }
 
-func EnsureAuth(c internal.VaultsmithClient) error {
-	// Ensure that all our auth types are enabled and have the correct configuration
-	authListLive, err := c.ListAuth()
-	if err != nil {
-		return err
-	}
-	log.Printf("live auths: %+v", authListLive)
-
-	// TODO hard-coded hack until we figure out how to structure the configuration
-	s, err := internal.ReadFile("example/sys/auth/approle.json")
-
-	var enableOpts vaultApi.EnableAuthOptions
-	err = json.Unmarshal([]byte(s), &enableOpts)
-	if err != nil {
-		return err
-	}
-
-	// we need to convert to AuthConfigOutput in order to compare with existing config
-	var enableOptsAuthConfigOutput vaultApi.AuthConfigOutput
-	enableOptsAuthConfigOutput, err = internal.ConvertAuthConfigInputToAuthConfigOutput(enableOpts.Config)
-	if err != nil {
-		return err
-	}
-
-	var authListConfigured map[string]*vaultApi.AuthMount
-	authListConfigured = make(map[string]*vaultApi.AuthMount)
-	approle := vaultApi.AuthMount{
-		Type:   "approle",
-		Config: enableOptsAuthConfigOutput,
-	}
-
-	authListConfigured["approle"] = &approle
-
-	// Iterate over the configured auths and ensure they are enabled with the correct config
-	for k, authMount := range authListConfigured {
-		// find in live list
-		// append slash because the config from server includes it
-		path := k + "/"
-		if liveAuth, ok := authListLive[path]; ok {
-			if isConfigApplied(enableOpts.Config, liveAuth.Config) {
-				log.Printf("Configuration for role %s already applied\n", authMount.Type)
-				continue
-			}
-		}
-		log.Printf("Enabling %s\n", authMount.Type)
-		c.EnableAuth(authMount.Type, &enableOpts)
-	}
-
-	for k, authMount := range authListLive {
-		// delete entries not in configured list
-		path := strings.Trim(k, "/")
-		if _, ok := authListConfigured[path]; ok {
-			// present, do nothing
-		} else if authMount.Type == "token" {
-			// cannot be disabled, would give http 400 if attempted
-		} else {
-			log.Printf("Disabling auth type %s\n", authMount.Type)
-			err := c.DisableAuth(authMount.Type)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-	return nil
-}
-
-// return true if the localConfig is reflected in remoteConfig, else false
-func isConfigApplied(localConfig vaultApi.AuthConfigInput, remoteConfig vaultApi.AuthConfigOutput) bool {
-	/*
-		AuthConfigInput uses string for int types, so we need to re-cast them in order to do a
-		comparison
-	*/
-
-	converted, err := internal.ConvertAuthConfigInputToAuthConfigOutput(localConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if reflect.DeepEqual(converted, remoteConfig) {
-		return true
-	} else {
-		return false
-	}
-}
-
 
 func Run(c internal.VaultsmithClient, config *VaultsmithConfig) error {
 	err := c.Authenticate(config.vaultRole)
@@ -163,20 +75,15 @@ func Run(c internal.VaultsmithClient, config *VaultsmithConfig) error {
 		return fmt.Errorf("failed authenticating with Vault: %s", err)
 	}
 
-	err = EnsureAuth(c)
+	err = internal.EnsureAuth(c)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//err = c.EnableAuth("approle", &approleOpts)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-
-	//internal.PutPoliciesFromDir("./example")
-	//if err != nil {
-	//	log.Fatal(fmt.Sprintf("Error writing policy: %s", err))
-	//}
+	internal.PutPoliciesFromDir("./example")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error writing policy: %s", err))
+	}
 
 	log.Println("Success")
 	return nil
